ecbdda4a6274e4d19b4ecd0d580c0b5a98fd7825
Whitey <c1783468@gmail.com>
2019-12-25 12:33:03 +0800

    Add sleep time

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const AutoCard = require('./autocard.js');
 
-try{
-    var User = require('./config.json');
-    User.forEach(data => {
-        new AutoCard(data);
-    });
-}
-catch(err){
-    if(err.message.search('Cannot find module') != -1){
-        console.log('No config file, using standard input');
-        new AutoCard();
+
+main();
+async function main(){
+    try{
+        var User = require('./config.json');
+        for(let i of User){
+            await getClient(i);
+            await sleep(7);
+        }
+
     }
-    else{
-        console.error(err.message);
+    catch(err){
+        if(err.message.search('Cannot find module') != -1){
+            console.log('No config file, using standard input');
+            new AutoCard();
+        }
+        else{
+            console.error(err.message);
+        }
     }
 }
 
+
+async function getClient(data) {
+    return new Promise(resolve=>{
+        new AutoCard(data);
+        resolve();
+    })
+}
+
+async function sleep(second) {
+    return new Promise(resolve=>{
+        setTimeout(resolve, second * 1000);
+    })
+}
