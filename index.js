5eca48dcb6a7e432d6fff4bb802af3b475338e6f
Whitey <c1783468@gmail.com>
2021-12-29 08:52:01 +0800

    add eslint

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const AutoCard = require('./autocard.js');
-const remain = parseInt(process.argv[2],10) || 3; 
+const remain = parseInt(process.argv[2], 10) || 1;
 
 main();
-async function main(){
-    try{
-        var User = require('./config.json');
-        for(let i of User){
-            await getClient(i);
-            await sleep(7);
-        }
+async function main() {
+  try {
+    var User = require('./config.json');
+    for (let i of User) {
+      await getClient(i);
+      await sleep(7);
+    }
 
+  }
+  catch (err) {
+    if (err.message.search('Cannot find module') != -1) {
+      console.log('No config file, using standard input');
+      new AutoCard();
     }
-    catch(err){
-        if(err.message.search('Cannot find module') != -1){
-            console.log('No config file, using standard input');
-            new AutoCard();
-        }
-        else{
-            console.error(err.message);
-        }
+    else {
+      console.error(err.message);
     }
+  }
 }
 
 
 async function getClient(data) {
-    return new Promise(resolve=>{
-        new AutoCard(data,remain);
-        resolve();
-    })
+  return new Promise(resolve => {
+    new AutoCard(data, remain);
+    resolve();
+  })
 }
 
 async function sleep(second) {
-    return new Promise(resolve=>{
-        setTimeout(resolve, second * 1000);
-    })
+  return new Promise(resolve => {
+    setTimeout(resolve, second * 1000);
+  })
 }
