0ef341b457b62aa85986f8c10f4d5bd34bd87ca1
Whitey <c1783468@gmail.com>
2022-07-06 22:25:31 +0800

    update login flow for user without shared secret

--- user.js
+++ user.js
@@ -11,29 +11,60 @@ module.exports = class SteamUser {
 
     this.data.account = this.data.account ? this.data.account : prompt('Enter your account: ');
     this.data.password = this.data.password ? this.data.password : prompt('Enter your password: ', { echo: '*' });
-    this.data.shared_secret = this.data.shared_secret ? SteamTotp.generateAuthCode(this.data.shared_secret) : prompt('Enter your auth code: ');
 
-    let logData = {
+    this.init();
+  }
+
+  login(loginData) {
+    return new Promise((resolve, reject) => {
+      this.community.login(loginData, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  }
+
+  async init() {
+    const loginData = {
       accountName: this.data.account,
       password: this.data.password,
-      twoFactorCode: this.data.shared_secret
     };
-    this.community.login(logData, (err) => {
-      if (err) {
-        console.error(`[${this.data.account}]`, err.message);
-        return;
+    loginData.twoFactorCode = this.data.shared_secret ? SteamTotp.generateAuthCode(this.data.shared_secret) : undefined;
+    try {
+      await this.login(loginData);
+      console.log(`${loginData.accountName} logged into steam`);
+      await this.loadMods();
+    } catch (err) {
+      if (err.message === 'SteamGuard') {
+        const authCode = prompt('Enter your auth code from email: ');
+        await this.login({ ...loginData, authCode });
+        console.log(`${loginData.accountName} logged into steam`);
+        await this.loadMods();
+      } else if (err.message === 'SteamGuardMobile') {
+        const twoFactorCode = prompt('Enter your auth code from mobile: ');
+        await this.login({ ...loginData, twoFactorCode });
+        console.log(`${loginData.accountName} logged into steam`);
+        await this.loadMods();
+      } else {
+        console.error(`[${this.data.account}]`, 'login failed ', err.message);
+        throw err;
       }
-      console.log(`${logData.accountName} logged into steam`);
-      this.loadMods();
-    });
+    }
+    
   }
 
   async loadMods() {
     for (let plugin of Plugins) {
       if (plugin.enable) {
-        console.log(`[${this.data.account}] Loading ${plugin.name}`);
+        console.log(`[${this.data.account}] Loading plugin: ${plugin.name}`);
         this.mods[plugin.name] = require(`./plugins/${plugin.script}`).bind(this);
-        await this.mods[plugin.name](plugin);
+        try {
+          await this.mods[plugin.name](plugin);
+        } catch (err) {
+          console.error(`[${this.data.account}]`, 'run plugin with error: ', err);
+        }
       }
     }
   }
