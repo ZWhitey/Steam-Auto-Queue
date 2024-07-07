2a48c0641fd9eb8eea3e0975fa7e944cdfe34eba
Whitey <c1783468@gmail.com>
2024-07-07 11:36:41 +0800

    rewrite login process for refresh token

--- user.js
+++ user.js
@@ -1,6 +1,8 @@
 const prompt = require('prompt-sync')();
 const SteamTotp = require('steam-totp');
 const SteamCommunity = require('steamcommunity');
+const NodeSteamUser = require('steam-user');
+const _ = require('lodash');
 const Plugins = require('./plugins.json');
 const Storage = require('./storage');
 const storage = new Storage();
@@ -11,95 +13,84 @@ module.exports = class SteamUser {
     this.mods = {};
     this.data = data ? data : {};
     this.community = new SteamCommunity();
+    this.user = new NodeSteamUser({ renewRefreshTokens: true });
 
-    this.data.account = this.data.account ? this.data.account : prompt('Enter your account: ');
-    this.data.password = this.data.password ? this.data.password : prompt('Enter your password: ', { echo: '*' });
+    this.data.account = this.data.account
+      ? this.data.account
+      : prompt('Enter your account: ');
 
-    const cookie = storage.getCookie(this.data.account);
-    if (cookie) {
-      this.initWithCookie(cookie);
+    this.refreshToken = storage.getRefreshToken(this.data.account);
+    if (_.isNil(this.refreshToken)) {
+      this.data.password = this.data.password
+        ? this.data.password
+        : prompt('Enter your password: ', { echo: '*' });
     } else {
-      this.init();
+      console.log(`[${this.data.account}] login with refresh token`);
     }
+    this.initSteamUser();
   }
 
-  async initWithCookie(cookie) {
-    this.community.setCookies(cookie);
-    if (await this.loggedIn()) {
-      console.log(`[${this.data.account}] login with cookie`);
-      await this.loadMods();
-    } else {
-      console.log(`[${this.data.account}] cookie expired try to relogin`);
-      storage.removeCookie(this.data.account);
-      await this.init();
-    }
-  }
-
-  async init() {
-    const loginData = {
-      accountName: this.data.account,
-      password: this.data.password,
-    };
-    loginData.twoFactorCode = this.data.shared_secret ? SteamTotp.generateAuthCode(this.data.shared_secret) : undefined;
-    try {
-      await this.login(loginData);
-      console.log(`${loginData.accountName} logged into steam`);
-      await this.loadMods();
-    } catch (err) {
-      if (err.message === 'SteamGuard') {
-        const authCode = prompt(`[${this.data.account}] Enter your auth code from email: `);
-        await this.login({ ...loginData, authCode });
-        console.log(`${loginData.accountName} logged into steam`);
-        await this.loadMods();
-      } else if (err.message === 'SteamGuardMobile') {
-        const twoFactorCode = prompt(`[${this.data.account}] Enter your auth code from mobile: `);
-        await this.login({ ...loginData, twoFactorCode });
-        console.log(`${loginData.accountName} logged into steam`);
-        await this.loadMods();
-      } else {
-        console.error(`[${this.data.account}]`, 'login failed ', err.message);
-        throw err;
+  async initSteamUser() {
+    const loginData = {};
+    if (_.isNil(this.refreshToken)) {
+      loginData.accountName = this.data.account;
+      loginData.password = this.data.password;
+      if (this.data.shared_secret) {
+        loginData.twoFactorCode = SteamTotp.generateAuthCode(
+          this.data.shared_secret
+        );
       }
+    } else {
+      loginData.refreshToken = this.refreshToken;
     }
+
+    this.user.logOn(loginData);
+
+    this.user.on('loggedOn', () => {
+      console.log(`[${this.data.account}] logged on steam`);
+    });
+
+    this.user.on('refreshToken', (token) => {
+      console.log(`[${this.data.account}] save refresh token`);
+      storage.saveRefreshToken(this.data.account, token);
+    });
+
+    this.user.on('error', (err) => {
+      console.error(`[${this.data.account}]`, err);
+    });
+
+    this.user.on('webSession', async (sessionID, cookies) => {
+      console.log(`[${this.data.account}] login web session`);
+      this.community.setCookies(cookies);
+      try {
+        this.loadMods();
+      } catch (err) {
+        console.error(`[${this.data.account}]`, 'load mods with error: ', err);
+      } finally {
+        this.end = true;
+        this.user.logOff();
+      }
+    });
   }
 
   async loadMods() {
     for (let plugin of Plugins) {
       if (plugin.enable) {
         console.log(`[${this.data.account}] Loading plugin: ${plugin.name}`);
-        this.mods[plugin.name] = require(`./plugins/${plugin.script}`).bind(this);
+        this.mods[plugin.name] = require(`./plugins/${plugin.script}`).bind(
+          this
+        );
         try {
           await this.mods[plugin.name](plugin);
         } catch (err) {
-          console.error(`[${this.data.account}]`, 'run plugin with error: ', err);
+          console.error(
+            `[${this.data.account}]`,
+            'run plugin with error: ',
+            err
+          );
         }
       }
     }
     this.end = true;
   }
-
-  login(loginData) {
-    return new Promise((resolve, reject) => {
-      this.community.login(loginData, (err, sid, cookies) => {
-        if (err) {
-          return reject(err);
-        }
-        storage.saveCookie(this.data.account, cookies);
-        return resolve();
-      });
-    });
-  }
-
-  loggedIn() {
-    return new Promise((resolve) => {
-      this.community.loggedIn((err, loggedIn) => {
-        if (err) {
-          return resolve(false);
-        }
-        return resolve(loggedIn);
-      });
-    });
-  }
-
-
 };
